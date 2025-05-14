46d3f120f73109ae6012a80f54df18132c9738d5
Vaishnavi Potdar <vaishnavi.potdar@sarvarth.com>
2025-05-14 21:20:57 +0530

    added all

---
+++ Basics of python/CHAPTER 2/03_operators.py
@@ -0,0 +1,28 @@
+# Arithametic Operators
+# a = 4
+# b = 7
+# c = 9
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+# print(a//b)
+
+# Assignment Operators
+c = 12
+x = 4-3 # assign 4-3 in a 
+c += 3  # Increment value of b by 3 
+c -= 3  # Decrement value of b by 3 
+c *= 3
+c /= 3
+
+# Comparison Operators 
+a = 4>2
+a = 4<2
+a = 5>=5
+a = 4==4
+a = 4!=4
+
+
+
