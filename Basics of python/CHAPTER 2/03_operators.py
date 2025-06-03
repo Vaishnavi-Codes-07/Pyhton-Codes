24817cd6be5f37ee399842d0aa928ca50d1d0258
Vaishnavi Potdar <vaishnavi.potdar@sarvarth.com>
2025-06-03 13:13:46 +0530

    Add all remaining files and folders

--- Basics of python/CHAPTER 2/03_operators.py
+++ Basics of python/CHAPTER 2/03_operators.py
@@ -24,5 +24,26 @@ a = 5>=5
 a = 4==4
 a = 4!=4
 
+# Logical Operators 
 
+e = True or False 
+
+# Truth table of "or" 
+
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+
+# Truth table of "and"
+
+print("True and False is", True and False)
+print("True and True is", True and True)
+print("False and True is", False and True)
+print("False and False is", False and False)
+
+# Truth table of "not"
+
+print(not(True))
+print(not(False))
 
--- Basics of python/hello.py
+++ Basics of python/hello.py
@@ -1,7 +1,9 @@
-print("Hello World")
-print(20+30)
-print(45*32)
-print(67-62)
-print(23/2)
-print(36**3)
-print(78//2)
+class Person :
+    def __init__(self, name):
+        self.name = name
+    def printName(self):
+        print("Hello "+ self.name)
+
+
+p1 = Person("omkar")
+p1.printName()
